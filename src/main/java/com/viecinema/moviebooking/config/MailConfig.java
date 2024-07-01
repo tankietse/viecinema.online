9ad6bc20714b55856f6af1c5a5b0291b8ca94e85
Tan Kiet Nguyen <kiettuanleo@gmail.com>
2024-07-01 19:23:34 +0700

    Fixed layout

---
+++ src/main/java/com/viecinema/moviebooking/config/MailConfig.java
@@ -0,0 +1,32 @@
+package com.viecinema.moviebooking.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${tmdb.api.key}")
+    private String apiKey;
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.example.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("your_email@example.com");
+        mailSender.setPassword("your_email_password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
--- src/main/java/com/viecinema/moviebooking/controller/UserController.java
+++ src/main/java/com/viecinema/moviebooking/controller/UserController.java
@@ -1,18 +1,62 @@
 package com.viecinema.moviebooking.controller;
 
+import com.viecinema.moviebooking.model.User;
+import com.viecinema.moviebooking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/login")
     public String login(@RequestParam(required = false) String error, Model model) {
         if (error != null) {
             model.addAttribute("error", "Invalid username or password");
         }
-        return "login";
+        return "user/login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@RequestParam String username, @RequestParam String password, @RequestParam String confirmPassword, Model model) {
+        if (!password.equals(confirmPassword)) {
+            model.addAttribute("error", "Mật khẩu không khớp");
+            return "user/register";
+        }
+
+        if (userService.existsByUsername(username)) {
+            model.addAttribute("error", "Đã tồn tại user này");
+            return "user/register";
+        }
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        userService.saveUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/forgot-pass")
+    public String forgotPassword() {
+        return "user/forgot-pass";
+    }
+
+    @PostMapping("/forgot-pass")
+    public String forgotPassword(@RequestParam String email, Model model) {
+        // Logic gửi email để reset mật khẩu
+
+        model.addAttribute("message", "Password reset instructions have been sent to your email.");
+        return "user/forgot-pass";
     }
 }
